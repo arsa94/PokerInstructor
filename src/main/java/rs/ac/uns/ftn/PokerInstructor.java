2844a23f93e4ae96182487170a1a72e82f3dd744
Milorad Vojnovic <milekuglas@gmail.com>
2017-07-01 22:17:40 +0200

    Scanner for input and fix some bugs

--- src/main/java/rs/ac/uns/ftn/PokerInstructor.java
+++ src/main/java/rs/ac/uns/ftn/PokerInstructor.java
@@ -13,90 +13,105 @@ import rs.ac.uns.ftn.exceptions.UndefinedStateException;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Micko on 29-Jun-17.
  */
 public class PokerInstructor {
     public static void main(String[] args) throws HandRankingException, UndefinedStateException {
-        Parser parser = new Parser();
-        JCommander.newBuilder()
-                .addObject(parser)
-                .build()
-                .parse(args);
 
-        System.out.println(parser);
+        boolean exit = false;
 
-        Card firstCard = parser.getCards().get(0);
-        Card secondCard = parser.getCards().get(1);
+        while(!exit){
+            Parser parser = new Parser();
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print(">> ");
+            String cmd = keyboard.nextLine();
+            if(cmd.equals("q")){exit = true; continue;}
+            String[] cmds = cmd.split(" ");
 
-        Deck deck = new Deck();
-        deck.removeCard(firstCard);
-        deck.removeCard(secondCard);
+            //-c ah,3h,2h,ks,jc,7s,kc -s t -p b -a af -pot 10000 -no 1
+
+            JCommander.newBuilder()
+                    .addObject(parser)
+                    .build()
+                    .parse(cmds);
+
+            System.out.println(parser);
+
+            Card firstCard = parser.getCards().get(0);
+            Card secondCard = parser.getCards().get(1);
+
+            Deck deck = new Deck();
+            deck.removeCard(firstCard);
+            deck.removeCard(secondCard);
 
 
-        GameState gameState = new GameState();
-        gameState.setPlayerPosition(parser.getPosition());
-        gameState.setActionsBeforePlayer(parser.getAction());
+            GameState gameState = new GameState();
+            gameState.setPlayerPosition(parser.getPosition());
+            gameState.setActionsBeforePlayer(parser.getAction());
 
-        int len = parser.getCards().size();
+            int len = parser.getCards().size();
 
-        if(len >= 5){
-            HashSet<Card> flop = new HashSet<Card>();
-            flop.add(parser.getCards().get(2));
-            flop.add(parser.getCards().get(3));
-            flop.add(parser.getCards().get(4));
-            deck.removeCard(parser.getCards().get(2));
-            deck.removeCard(parser.getCards().get(3));
-            deck.removeCard(parser.getCards().get(4));
-            gameState.setFlop(flop);
+            if(len >= 5){
+                HashSet<Card> flop = new HashSet<Card>();
+                flop.add(parser.getCards().get(2));
+                flop.add(parser.getCards().get(3));
+                flop.add(parser.getCards().get(4));
+                deck.removeCard(parser.getCards().get(2));
+                deck.removeCard(parser.getCards().get(3));
+                deck.removeCard(parser.getCards().get(4));
+                gameState.setFlop(flop);
+            }
+
+            if(len >= 6){
+                deck.removeCard(parser.getCards().get(5));
+                gameState.setTurn(parser.getCards().get(5));
+            }
+
+            if(len == 7){
+                deck.removeCard(parser.getCards().get(6));
+                gameState.setRiver(parser.getCards().get(6));
+            }
+
+            List<Card> remainingCards = deck.getCards();
+            List<List<Double>> weightArray = HandRanker.getUniformWeightArray();
+
+            Player player = new Player(firstCard, secondCard, gameState);
+            if(parser.getState() != GameState.ROUND.PREFLOP){
+                player.calculateHandPotential(weightArray, parser.getNumberOfOpponents(), true, remainingCards);
+            }
+
+            System.out.println("HandStrength: " + player.getHandStrength());
+            System.out.println("Positive Potential: " + player.getPositiveHandPotential());
+            System.out.println("Negative Potential: " + player.getNegativeHandPotential());
+            System.out.println("ECH: " + player.getECH());
+
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kc = ks.getKieClasspathContainer();
+            KieSession kSession;
+            switch(parser.getState()){
+                case PREFLOP:   kSession = kc.newKieSession("preflop-rules");
+                    break;
+                case FLOP:      kSession = kc.newKieSession("flop-rules");
+                    break;
+                case TURN:      kSession = kc.newKieSession("flop-rules");
+                    break;
+                case RIVER:     kSession = kc.newKieSession("flop-rules");
+                    break;
+                default:        throw new UndefinedStateException();
+            }
+
+            kSession.insert(player);
+
+            System.out.println('\n');
+            int firedRules = kSession.fireAllRules();
+            if(firedRules == 0){
+                System.out.println("Bad cards - Fold");
+            }
+
+            System.out.println(firedRules);
         }
-
-        if(len >= 6){
-            deck.removeCard(parser.getCards().get(5));
-            gameState.setTurn(parser.getCards().get(5));
-        }
-
-        if(len == 7){
-            deck.removeCard(parser.getCards().get(6));
-            gameState.setRiver(parser.getCards().get(6));
-        }
-
-        List<Card> remainingCards = deck.getCards();
-        List<List<Double>> weightArray = HandRanker.getUniformWeightArray();
-
-        Player player = new Player(firstCard, secondCard, gameState);
-        player.calculateHandPotential(weightArray, parser.getNumberOfOpponents(), true, remainingCards);
-
-        System.out.println("HandStrength: " + player.getHandStrength());
-        System.out.println("Positive Potential: " + player.getPositiveHandPotential());
-        System.out.println("Negative Potential: " + player.getNegativeHandPotential());
-        System.out.println("ECH: " + player.getECH());
-
-        KieServices ks = KieServices.Factory.get();
-        KieContainer kc = ks.getKieClasspathContainer();
-        KieSession kSession;
-        switch(parser.getState()){
-            case PREFLOP:   kSession = kc.newKieSession("preflop-rules");
-                            break;
-            case FLOP:      kSession = kc.newKieSession("flop-rules");
-                            break;
-            case TURN:      kSession = kc.newKieSession("flop-rules");
-                            break;
-            case RIVER:     kSession = kc.newKieSession("flop-rules");
-                            break;
-            default:        throw new UndefinedStateException();
-        }
-
-        kSession.insert(player);
-
-        System.out.println('\n');
-        int firedRules = kSession.fireAllRules();
-        if(firedRules == 0){
-            System.out.println("Bad cards - Fold");
-        }
-
-        System.out.println(firedRules);
-
     }
 }
